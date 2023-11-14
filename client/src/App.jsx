fea2b48a47468f264abd47350f2855816ac17e3d
Raya Koleva <raya.koleva@abv.bg>
2023-11-14 19:23:14 +0200

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import { Routes, Route } from "react-router-dom";
+import Footer from './components/Footer.jsx'
+import Header from './components/Header.jsx'
+import Login from "./components/login/login.jsx";
+import Nagivation from "./components/Navigation.jsx";
+
+
+function App() {
+
+  return (
+    <>
+    <Nagivation/>
+    <Header/>
+    <Footer/>
+    </>
+    
+     
+   
+    
+
+
+  )
+}
+
+export default App
